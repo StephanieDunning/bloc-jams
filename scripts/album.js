b52d7045e221a2925c70b5db01de5b2c08e234a9
Stephanie Dunning <eggandcheesesandwich@gmail.com>
2016-09-20 19:18:14 -0700

    assign27-DOM-PlayPause2: done

--- scripts/album.js
+++ scripts/album.js
@@ -62,9 +62,14 @@ var setCurrentAlbum = function(album) {
 var findParentByClassName = function(element, targetClass) {
 	if (element) {
     	var currentParent = element.parentElement;
-        while (currentParent.className != targetClass && currentParent.className !== null) {
-			currentParent = currentParent.parentElement;
+        while (currentParent.className != targetClass ) {
+			currentParent = currentParent.parentElement;     
         }
+        if (currentParent === null) {
+            alert("No parent found");
+        } else if (currentParent.className != targetClass) {
+            alert("No parent found with that class name");
+        }       
         return currentParent;
     }
 };
